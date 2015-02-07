9e3662b05bdbad3b78607fc7230b8214e650f2a6
Kyle Mitchell <kyle@kemitchell.com>
2015-02-07 21:05:49 +0000

    include public library by default

--- source/configuration.js
+++ source/configuration.js
@@ -1,4 +1,10 @@
 var Configstore = require('configstore');
 var packageJSON = require('../package');
 
-module.exports = new Configstore(packageJSON.name, {libraries:{}});
+var DEFAULTS = {
+  libraries: {
+    'public': 'https://anonymous:anonymous@api.commonform.org'
+  }
+};
+
+module.exports = new Configstore(packageJSON.name, DEFAULTS);
