ac577bd690423cac0674861af3f303ae6b4bd6cb
Kyle Mitchell <kyle@kemitchell.com>
2015-09-03 16:34:00 -0700

    Add resolutions-schedules-exhibits numbering

--- source/numberings.js
+++ source/numberings.js
@@ -1,4 +1,5 @@
 module.exports = {
+  rse: 'resolutions-schedules-exhibits-numbering',
   ase: 'agreement-schedules-exhibits-numbering',
   decimal: 'decimal-numbering',
   outline: 'outline-numbering' }
