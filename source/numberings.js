9848d3ea5c3c1fe07e9d64d6821b93252cc43cce
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-09 14:15:59 -0800

    Add plan-addenda-exhibits-numbering

--- source/numberings.js
+++ source/numberings.js
@@ -1,5 +1,6 @@
 module.exports = {
   rse: 'resolutions-schedules-exhibits-numbering',
   ase: 'agreement-schedules-exhibits-numbering',
+  pae: 'plan-addenda-exhibits-numbering',
   decimal: 'decimal-numbering',
   outline: 'outline-numbering' }
